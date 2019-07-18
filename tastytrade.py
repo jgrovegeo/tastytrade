9a9c0af23ba1bdd2dacadc6896547e2be7561af2
Jared Grove <37494097+jgrovedev@users.noreply.github.com>
2019-07-18 14:45:38 +0000

    Add files via upload
    added debit/credit command and debit table

--- tastytrade.py
+++ tastytrade.py
@@ -22,27 +22,42 @@ while True:
         date_obj = datetime.strptime(date,'%Y-%m-%d').date()
         sym = input('Ticker: ')
         ivr = input('IVR: ')
+        premty = input('Premium Type (credit or debit): ')
         prem = float(input('Premium: '))
         exp = input('Expiration: ')
         exp_obj = datetime.strptime(exp,'%Y-%m-%d').date()
         print('\n')
         print('Exit trade 21 days prior to expiration:', exp_obj + timedelta(days=-21))
         print('\n')
-        table = tabulate([['10%', round(prem - (prem*.10), 2), '3', date_obj + timedelta(days=3)], 
-                        ['20%', round(prem - (prem*.20), 2), '6', date_obj + timedelta(days=6)],
-                        ['30%', round(prem - (prem*.30), 2), '10', date_obj + timedelta(days=10)],
-                        ['40%', round(prem - (prem*.40), 2), '15', date_obj + timedelta(days=15)],
-                        ['50%', round(prem - (prem*.50), 2), '19', date_obj + timedelta(days=19)],
-                        ['60%', round(prem - (prem*.60), 2), '24', date_obj + timedelta(days=24)]],
-                        headers=['% of Max Profit', 'Premium', 'Avg. Days Held', 'Date'], 
-                        tablefmt='orgtbl')
-        print(table)
+        
+        if premty == 'credit':
+            table = tabulate([['10%', round(prem - (prem*.10), 2), '3', date_obj + timedelta(days=3)], 
+                            ['20%', round(prem - (prem*.20), 2), '6', date_obj + timedelta(days=6)],
+                            ['30%', round(prem - (prem*.30), 2), '10', date_obj + timedelta(days=10)],
+                            ['40%', round(prem - (prem*.40), 2), '15', date_obj + timedelta(days=15)],
+                            ['50%', round(prem - (prem*.50), 2), '19', date_obj + timedelta(days=19)],
+                            ['60%', round(prem - (prem*.60), 2), '24', date_obj + timedelta(days=24)]],
+                            headers=['% of Max Profit', 'Premium', 'Avg. Days Held', 'Date'], 
+                            tablefmt='orgtbl')
+            print(table)
+        
+        if premty == 'debit':
+            table = tabulate([['10%', round(prem + (prem*.10), 2), '3', date_obj + timedelta(days=3)], 
+                            ['20%', round(prem + (prem*.20), 2), '6', date_obj + timedelta(days=6)],
+                            ['30%', round(prem + (prem*.30), 2), '10', date_obj + timedelta(days=10)],
+                            ['40%', round(prem + (prem*.40), 2), '15', date_obj + timedelta(days=15)],
+                            ['50%', round(prem + (prem*.50), 2), '19', date_obj + timedelta(days=19)],
+                            ['60%', round(prem + (prem*.60), 2), '24', date_obj + timedelta(days=24)]],
+                            headers=['% of Max Profit', 'Premium', 'Avg. Days Held', 'Date'], 
+                            tablefmt='orgtbl')
+            print(table)            
 
         # WRITES DATA TO A TEXT FILE
         with open('/home/jared/Desktop/positions.txt', 'a') as f:
             f.write('\n')
             f.write('Symbol: ' + str(sym) + '\n')
             f.write('IV rank: ' + str(ivr) + '\n')
+            f.write('Premium type: ' + premty + '\n')
             f.write('Premium collected: ' + str(prem) + '\n')
             f.write('Date filled: ' + str(date_obj) + '\n')
             f.write('Exit by: ' + str(exp_obj + timedelta(days=-21)) + '\n')
